f486e89e0d147d6c49006ce301e7c5e19143d522
Sain-Biswas <sain.biswas614@gmail.com>
2023-09-08 17:03:40 +0530

    changed favicon and other logos to new logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import NavBar from './Components/NavBar/NavBar';
 
 function App() {
